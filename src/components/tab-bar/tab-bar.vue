45fccfc5f51b3a7b6a6230e7c3ee3068a6164b76
ekingin <1321557725@qq.com>
2022-10-28 14:38:58 +0800

    tabbar修改

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -4,33 +4,25 @@
   import { getLocalImgUrl } from '@/utils'
 
   let currentIndex = ref(0)
+
 </script>
 
 <template>
-  <div class="tab-bar">
-    <van-tabbar v-model="currentIndex" active-color="#ff9854">
-      <template v-for="(menu, index) in tabbarData" :key="menu.path">
-        <van-tabbar-item :to="menu.path">
-          <span>{{ menu.text }}</span>
-          <template #icon="props">
-            <img v-if="currentIndex === index" class="icon" :src="getLocalImgUrl(menu.imgActive)" alt="">
-            <img v-else class="icon" :src="getLocalImgUrl(menu.img)" alt="">
-          </template>
-        </van-tabbar-item>
-      </template>
-    </van-tabbar>
-  </div>
+  <van-tabbar class="tab-bar" v-model="currentIndex" active-color="#ff9854" route>
+    <template v-for="(menu, index) in tabbarData" :key="menu.path">
+      <van-tabbar-item :to="menu.path">
+        <span>{{ menu.text }}</span>
+        <template #icon="props">
+          <img v-if="props.active" class="icon" :src="getLocalImgUrl(menu.imgActive)" alt="">
+          <img v-else class="icon" :src="getLocalImgUrl(menu.img)" alt="">
+        </template>
+      </van-tabbar-item>
+    </template>
+  </van-tabbar>
 </template>
 
 <style lang="less" scoped>
   .tab-bar {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 50px;
-    display: flex;
-    border-top: 1px solid #f3f3f3;
     .icon {
       height: 30px;
     }
